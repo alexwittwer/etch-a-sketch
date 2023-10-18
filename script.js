03e2fc054433714dac8d715bb6a613a49458ce24
alexwittwer <wittweral@gmail.com>
2023-10-17 21:02:23 -0400

    create working prototype

---
+++ script.js
@@ -0,0 +1,44 @@
+//CONSTANTS
+const gridWindow = document.querySelector("#gridWindow");
+
+//Function 1x1:           Makes n divs, append
+//Requirements:           none
+
+//probably best to call this function with a button rather than input
+function makeSquareDiv(nums) {
+  for (let i = 0; i < nums; i++) {
+    let squareDiv = document.createElement("div");
+    squareDiv.setAttribute("class", "squareDiv");
+    gridWindow.appendChild(squareDiv);
+  }
+}
+
+function sqrt(nums) {
+  return Math.sqrt(nums);
+}
+
+makeSquareDiv(16);
+
+const squarePtr = document.querySelectorAll(".squareDiv");
+squarePtr.forEach((item) => {
+  item.addEventListener("mouseover", () => {
+    item.style.backgroundColor = "black";
+  });
+});
+
+//Function set stye:
+function setSquareStyle() {}
+//End function
+
+//Function Grid Clone:    Duplicates 1x1 function into a square
+//                        eg 16x16 grid with hover elements
+//Requirements:           Should populate the grid with N * N elements
+//                        and we will let flexbox handle the rest
+//                        This way, we stay in O(n) time
+
+//End function
+
+//Function reset:         Resets all hover elements to white space
+//Requirements:           Should not require a page reload.
+
+//End function
