4d7c952745d65d7c65dfce19c318c2dddc5e5cac
alexwittwer <wittweral@gmail.com>
2023-10-17 21:57:20 -0400

    refactor for readability

--- script.js
+++ script.js
@@ -1,33 +1,14 @@
 //CONSTANTS
 const gridWindow = document.querySelector("#gridWindow");
 
-//Function 1x1:           Makes n divs, append
-//Requirements:           none
-
-//probably best to call this function with a button rather than input
-function makeSquareDiv(nums) {
-  for (let i = 0; i < nums ** 2; i++) {
-    let squareDiv = document.createElement("div");
-    squareDiv.setAttribute("class", "squareDiv");
-    gridWindow.appendChild(squareDiv);
-  }
-}
-
-function promptUser() {
-  let value = Math.floor(prompt("Enter a number between 16 and 100"));
-  if (isNaN(value)) {
-    alert("Unknown entry");
-    promptUser();
-  } else {
-    return value;
-  }
-}
 let nums = promptUser();
+
 //when using height width in css, set to 100 / nums
 let numsCalc = 100 / nums;
-
+//creates the grid
 makeSquareDiv(nums);
 
+//updates squareDiv properties for mouseover, sets height/width to ensure correct spacing
 const squarePtr = document.querySelectorAll(".squareDiv");
 squarePtr.forEach((item) => {
   item.style.height = `${numsCalc}%`;
@@ -37,19 +18,22 @@ squarePtr.forEach((item) => {
   });
 });
 
-//Function set stye:
-function setSquareStyle() {}
-//End function
+//Function: creates a user-defined number of "square" divs that will become boxes
+function makeSquareDiv(nums) {
+  for (let i = 0; i < nums ** 2; i++) {
+    let squareDiv = document.createElement("div");
+    squareDiv.setAttribute("class", "squareDiv");
+    gridWindow.appendChild(squareDiv);
+  }
+}
 
-//Function Grid Clone:    Duplicates 1x1 function into a square
-//                        eg 16x16 grid with hover elements
-//Requirements:           Should populate the grid with N * N elements
-//                        and we will let flexbox handle the rest
-//                        This way, we stay in O(n) time
-
-//End function
-
-//Function reset:         Resets all hover elements to white space
-//Requirements:           Should not require a page reload.
-
-//End function
+//get user input
+function promptUser() {
+  let value = Math.floor(prompt("Enter a number between 16 and 100"));
+  if (isNaN(value)) {
+    alert("Unknown entry");
+    promptUser();
+  } else {
+    return value;
+  }
+}
