0458d25d46d2e8ce6c9920b89e8e13d9d219d9df
Alex Wittwer <wittweral@gmail.com>
2023-10-18 16:57:11 -0400

    add UI controls

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 //CONSTANTS
 const gridWindow = document.querySelector("#gridWindow");
+const resetBtn = document.querySelector(".resetButton");
+const inputValue = document.querySelector("input");
 
-let nums = promptUser();
-
+let nums = inputValue.value;
 //when using height width in css, set to 100 / nums
 let numsCalc = 100 / nums;
+
 //creates the grid
-makeSquareDiv(nums);
+resetSketch();
 
 //updates squareDiv properties for mouseover, sets height/width to ensure correct spacing
 const squarePtr = document.querySelectorAll(".squareDiv");
 
+//sets an active listener to the reset button
+resetBtn.addEventListener("click", () => {
+  resetSketch();
+});
+
 //Function: creates a user-defined number of "square" divs that will become boxes
-function makeSquareDiv(nums) {
+function makeSquareDiv(nums, numsCalc) {
   for (let i = 0; i < nums ** 2; i++) {
     let squareDiv = document.createElement("div");
     squareDiv.setAttribute("class", "squareDiv");
@@ -25,13 +32,14 @@ function makeSquareDiv(nums) {
   }
 }
 
-//get user input
-function promptUser() {
-  let value = Math.floor(prompt("Enter a number between 16 and 100"));
-  if (isNaN(value)) {
-    alert("Unknown entry");
-    promptUser();
-  } else {
-    return value;
-  }
+function resetSketch() {
+  let nums = inputValue.value;
+  let numsCalc = 100 / nums;
+  //remove all grids
+  let squareDivPtr = document.querySelectorAll(".squareDiv");
+  squareDivPtr.forEach((item) => {
+    item.remove();
+  });
+  //rebuild with new input value
+  makeSquareDiv(nums, numsCalc);
 }
